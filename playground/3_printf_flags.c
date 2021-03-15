9f76020222d86163e98a393751709688748d50f3
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-03-15 10:10:09 +0000

    updates flags playground 🇵🇹

--- playground/3_printf_flags.c
+++ playground/3_printf_flags.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
+/* %d flag */
 int		main()
 {
 	int x = 123456783;
 	int y = 12;
-	// if length of int is x, width will offset by n units if length = x' + n
-	printf("%06.2d\n", y);
-	printf("%0.6d\n", y);
-	printf("%06d\n", y);
-	printf("%0*9d\n", 1, 2);
+	// if length of int is x = x' + n, width will offset by n units
+
+	// offsets 4 units to the right (provides a block of 6 units)
+	printf("W | width is 6 -- offsets 4 units to the right ⏬\n");
+	printf("%6d", y);
+	printf("\n-------\n");
+	printf("W0 | the same, but whitespace is now 0 ⏬\n");
+	printf("%06d", y);
+	printf("\n-------\n");
+	printf("W0. | ⚠️ \n");
+	printf("%06.1d", y);
+	printf("\n-------\n");
+	printf("- | uses flag to keep left ⏬\n");
+	printf("%-6d", y);
+	printf("\n-------\n");
+	printf("* | first argument is W, next is d content ⏬\n");
+	printf("%*d", 4, 4);
+	printf("\n-------\n");
+	printf("0* | same, but with 0 ⏬\n");
+	printf("%0*d", 2, 4);
+	printf("\n-------\n");
+	printf("-* | ⚠️  adjusts to the left ⏬\n");
+	printf("%-*d", 4, 4);
+	printf("\n-------\n");
+	printf("*2$ | ⚠️  using 2nd argument (1, 4, 2) ⏬\n");
+	printf("1st: %*2$d\n2nd: %d\n3rd: %d", 1, 4, 2);
+	printf("\n-------\n");
 }
