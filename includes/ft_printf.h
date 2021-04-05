9f41030e84f40bb6e3f86139c7aa910d4aa411a5
apinto <apinto@42lisboa.com>
2021-04-05 16:54:47 +0100

    almost forgot header norm 🙀

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,21 +6,23 @@
 /*   By: apinto <apinto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 04:05:17 by apinto            #+#    #+#             */
-/*   Updated: 2021/04/05 10:38:00 by apinto           ###   ########.fr       */
+/*   Updated: 2021/04/05 16:53:54 by apinto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include "../libft/libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <limits.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "../libft/libft.h"
 
-#define TYPES "cspdiuxX"
-#define DEC_BASE "0123456789"
-#define HEX_BASE "0123456789abcdef"
-#define BUFF_LEN 20
+# define TYPES "cspdiuxX"
+# define DEC_BASE "0123456789"
+# define HEX_BASE "0123456789abcdef"
+# define BUFF_LEN 20
 
-typedef struct	s_info{
+typedef struct s_info{
 	int				zero;
 	int				nbr_zeros;
 	int				minus;
@@ -43,7 +45,11 @@ int				writes_chars(char *begg, char *str);
 void			initializes_info(t_info *info);
 void			handles_conversion(va_list *pargs, char **str);
 void			cleans_info_with_prios(t_info *info);
-void			int_family_allocation(t_info *info, va_list *pargs, char *buffer);
+void			int_family_allocation(t_info *info,
+					va_list *pargs, char *buffer);
 void			char_family_allocation(t_info *info, va_list *pargs);
-void			number_to_string(t_info *info, unsigned long long content, char *base, char *buffer);
+void			number_to_string(t_info *info,
+					unsigned long long content, char *base, char *buffer);
 void			writes_buffer(t_info *info);
+
+#endif
